773b3ba5753f928fa6423331914ae87014b54f0b
山越貴耀 <takateru@vpn12-client-172-20-219-3.princeton.edu>
2020-11-12 17:30:42 +0900

    added dep_norm analysis

--- CountFreq.py
+++ CountFreq.py
@@ -40,42 +40,52 @@ def ExtractFreq(id,metric,corpus):
     ShortDep = ""
     for line in doc:
         if len(list(line.sents)) == 1:
-            if len(line) == 10:
-                sent_num += 1
+            sent_num += 1
+            if metric in ['dep','dep_norm']:
+                total_dist = np.array([abs(token_pos-token.head.i) for token_pos,token in enumerate(line)]).sum()
                 if metric == 'dep':
-                    total_dist = np.array([abs(token_pos-token.head.i) for token_pos,token in enumerate(line)]).sum()
                     if total_dist in Freq:
                         Freq[total_dist] += 1
                     else:
                         Freq[total_dist] = 1
-                    if total_dist == 10:
-                        ShortDep += line.text+'\n'
-                else:
-                    for token in line:
-                        if metric in ['vocab', 'pos', 'tag']:
-                            if metric == 'vocab':
-                                word = token.text
-                            elif metric == 'pos':
-                                word = token.pos_
-                            elif metric == 'tag':
-                                word = token.tag_
-                            if word in Freq:
-                                Freq[word] += 1
+                elif metric == 'dep_norm':
+                    seq_len = len(line)
+                    if seq_len in Freq:
+                        if total_dist in Freq[seq_len]:
+                            Freq[seq_len][total_dist] += 1
+                        else:
+                            Freq[seq_len][total_dist] = 1
+                    else:
+                        Freq[seq_len] = {}
+                        Freq[seq_len][total_dist] = 1
+                if total_dist == 10:
+                    ShortDep += line.text+'\n'
+            else:
+                for token in line:
+                    if metric in ['vocab', 'pos', 'tag']:
+                        if metric == 'vocab':
+                            word = token.text
+                        elif metric == 'pos':
+                            word = token.pos_
+                        elif metric == 'tag':
+                            word = token.tag_
+                        if word in Freq:
+                            Freq[word] += 1
+                        else:
+                            Freq[word] = 1
+                    elif metric in ['pos_vocab', 'tag_vocab']:
+                        if metric == 'pos_vocab':
+                            word = token.pos_
+                        elif metric == 'tag_vocab':
+                            word = token.tag_
+                        if word in Freq:
+                            if token.text in Freq[word]:
+                                Freq[word][token.text] += 1
                             else:
-                                Freq[word] = 1
-                        elif metric in ['pos_vocab', 'tag_vocab']:
-                            if metric == 'pos_vocab':
-                                word = token.pos_
-                            elif metric == 'tag_vocab':
-                                word = token.tag_
-                            if word in Freq:
-                                if token.text in Freq[word]:
-                                    Freq[word][token.text] += 1
-                                else:
-                                    Freq[word][token.text] = 1
-                            else:
-                                Freq[word] = {}
                                 Freq[word][token.text] = 1
+                        else:
+                            Freq[word] = {}
+                            Freq[word][token.text] = 1
     if corpus == 'wiki':
         with open(f'../WikiData/10WordSents/CountFiles/{metric.upper()}Freq{id}.pkl','wb') as f:
             pickle.dump(Freq,f)
@@ -95,7 +105,7 @@ nlp.add_pipe(sentencizer,first=True)
 corpus = args[1]
 metric = args[2]
 assert corpus in ['wiki', 'bert'], 'Invalid corpus name'
-assert metric in ['vocab', 'pos', 'tag', 'dep', 'pos_vocab', 'tag_vocab'], 'Invalid metric name'
+assert metric in ['vocab', 'pos', 'tag', 'dep', 'pos_vocab', 'tag_vocab', 'dep_norm'], 'Invalid metric name'
 
 if corpus == 'wiki':
     arg = [(folder_name,metric,corpus) for folder_name in folder_name_list]
@@ -119,7 +129,7 @@ if metric in ['vocab', 'pos', 'tag', 'dep']:
                 FreqDictAll[word] += Dict[word]
             else:
                 FreqDictAll[word] = Dict[word]
-elif metric in ['pos_vocab', 'tag_vocab']:
+elif metric in ['pos_vocab', 'tag_vocab', 'dep_norm']:
     for Dict in DictList:
         for word in Dict:
             for token_text in Dict[word]:
@@ -134,14 +144,14 @@ elif metric in ['pos_vocab', 'tag_vocab']:
 if corpus == 'wiki':
     with open(f'../WikiData/10WordSents/{metric.upper()}FreqAll.pkl','wb') as f:
         pickle.dump(FreqDictAll,f)
-    if metric == 'dep':
+    if metric in ['dep', 'dep_norm']:
         with open('../WikiData/10WordSents/ShortDepSents.txt','w') as f:
             for sentence in ShortDepSent:
                 f.write(sentence)
 elif corpus == 'bert':
     with open(f'datafile/{metric.upper()}FreqAllBert.pkl','wb') as f:
         pickle.dump(FreqDictAll,f)
-    if metric == 'dep':
+    if metric in ['dep', 'dep_norm']:
         with open('datafile/ShortDepSentsBert.txt','w') as f:
             for sentence in ShortDepSent:
                 f.write(sentence)
--- PlotDistBootstrap.py
+++ PlotDistBootstrap.py
@@ -19,10 +19,15 @@ with open(f'datafile/{metric.upper()}FreqAllBert.pkl','rb') as f:
     bert_all = pickle.load(f)
 
 def Bootstrap(init_dict,iter_num,metric,specified_order):
-    init_list = []
-    for word,value in init_dict.items():
-        init_list.extend([word for i in range(value)])
-    return np.array([CreatePlotListFromDict(Counter(resample(init_list)),metric,specified_order) for i in range(iter_num)])
+    if metric in ['pos','tag','dep']:
+        init_list = []
+        for word,value in init_dict.items():
+            init_list.extend([word for i in range(value)])
+        return np.array([CreatePlotListFromDict(Counter(resample(init_list)),metric,specified_order) for i in range(iter_num)])
+    elif metric == 'dep_norm':
+        init_list = CreateListNorm(init_dict)
+        return np.array([CreateHist(resample(init_list)) for i in range(iter_num)])
+
 
 def CreatePlotListFromDict(init_dict,metric,specified_order):
     value_sum = np.array(list(init_dict.values())).sum()
@@ -31,14 +36,23 @@ def CreatePlotListFromDict(init_dict,metric,specified_order):
         new_array = np.zeros(max_key+1)
         for key,value in init_dict.items():
             new_array[key] = value/value_sum
-    else:
+    elif metric in ['pos', 'tag']:
         new_array = []
         for word in specified_order:
             new_array.append(init_dict[word]/value_sum)
         new_array = np.array(new_array)
     return new_array
 
-assert metric in ['pos','tag','dep'], 'Invalid metric name'
+def CreateHist(init_list):
+    return np.histogram(init_list,bins=np.arange(np.max(init_list),step=0.02),density=True)
+def CreateListNorm(init_dict):
+    init_list = []
+    for seq_len,seq_len_dict in init_dict.items():
+        for total_dist,value in seq_len_dict.items():
+            init_list.extend([total_dist/seq_len**2 for i in range(value)])
+    return init_list
+
+assert metric in ['pos','tag','dep','dep_norm'], 'Invalid metric name'
 color_list = sns.color_palette('Set2')
 iter_num = 20
 if metric == 'dep':
@@ -53,6 +67,18 @@ if metric == 'dep':
         plt.plot(np.arange(len(wiki_bootstrap_iter))+1,wiki_bootstrap_iter,alpha=0.5,color=color_list[2])
         plt.plot(np.arange(len(bert_bootstrap_iter))+1,bert_bootstrap_iter,alpha=0.5,color=color_list[3])
     plt.xscale('log')
+    plt.legend()
+elif metric == 'dep_norm':
+    wiki_all_plot = CreateHist(CreateListNorm(wiki_all))
+    bert_all_plot = CreateHist(CreateListNorm(bert_all))
+    wiki_bootstrap = Bootstrap(wiki_all,iter_num,'dep_norm',None)
+    bert_bootstrap = Bootstrap(bert_all,iter_num,'dep_norm',None)
+    fig = plt.figure(figsize=(10,5),dpi=200)
+    plt.plot(wiki_all_plot[1][:-1],wiki_all_plot[0],label='wiki',color=color_list[0])
+    plt.plot(bert_all_plot[1][:-1],bert_all_plot[0],label='bert',color=color_list[1])
+    for wiki_bootstrap_iter, bert_bootstrap_iter in zip(wiki_bootstrap,bert_bootstrap):
+        plt.plot(wiki_bootstrap_iter[1][:-1],wiki_bootstrap_iter[0],alpha=0.5,color=color_list[2])
+        plt.plot(bert_bootstrap_iter[1][:-1],bert_bootstrap_iter[0],alpha=0.5,color=color_list[3])
     plt.legend()
 else:
     key_sets = [set(wiki_all.keys()), set(bert_all.keys())]
