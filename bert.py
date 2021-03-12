686cfc243de0ed7fb1cfcb5a0523fd251903de89
山越貴耀 <takateru@yamakoshinoMacBook-Pro.local>
2021-03-12 19:48:51 +0900

    added revised multi-site MH sampling

--- bert.py
+++ bert.py
@@ -93,7 +93,7 @@ class UniformGibbs() :
         edit_loc = new_sentences[:,pos]!=sentences[:,pos]
         return new_sentences, edit_loc
 
-    def mask_prob(self, position) :
+    def mask_prob(self, position):
         """
         Predict probability of words at mask position
         """
@@ -102,7 +102,7 @@ class UniformGibbs() :
         outputs = self.model(masked_sentences)
         return F.softmax(outputs[0][:, position] / self.temp, dim = -1)
 
-    def get_total_likelihood(self) :
+    def get_total_likelihood(self):
         sent_probs = torch.zeros(self.sentences.shape).to(args.device)
 
         # Why cut off first and last?
@@ -129,25 +129,24 @@ class MultiSiteMH() :
         seq_len = self.sentences.shape[1]
         
         # exclude first/last tokens (CLS/SEP) from positions
-        mask_pos_list = torch.randperm(seq_len - 2)[:self.num_masks]+1
-        probs = self.mask_prob(mask_pos_list)
+        mask_pos_list = (torch.randperm(seq_len - 2)[:self.num_masks]+1).to(args.device)
+        probs = self.mask_prob(mask_pos_list,self.sentences)
         self.sentences, edit_rate = self.sample_words(probs, mask_pos_list, self.sentences)
         self.edit_rate = edit_rate.mean(axis=1)
     
     def sample_words(self, probs, pos, sentences):
-        old_sent_prob = self.get_total_likelihood()
+        old_joint_prob = self.get_joint_probability(pos,sentences)
         old_words = sentences[:,pos]
-        #Propose a pair of words
+        #Propose a set of words
         new_words = torch.tensor([list(torch.multinomial(prob, 1).squeeze(-1)) for prob in probs]).to(args.device)
         new_sentences = sentences.clone()
         new_sentences[:,pos] = new_words
-        self.sentences = new_sentences
-        new_sent_prob = self.get_total_likelihood()
+        new_joint_prob = self.get_joint_probability(pos,new_sentences)
         fwd_prob = torch.tensor([[torch.log(prob[word]).item() for word,prob in zip(word_list,prob_array)]\
                                      for word_list,prob_array in zip(new_words,probs)]).sum(axis=1).to(args.device)
         bck_prob = torch.tensor([[torch.log(prob[word]).item() for word,prob in zip(word_list,prob_array)]\
                                      for word_list,prob_array in zip(old_words,probs)]).sum(axis=1).to(args.device)
-        alpha = torch.exp(new_sent_prob - old_sent_prob + bck_prob - fwd_prob)
+        alpha = torch.exp(new_joint_prob - old_joint_prob + bck_prob - fwd_prob)
         alpha[alpha>1] = 1
         accept = torch.rand(sentences.shape[0]).to(args.device)<alpha
         chosen_words = old_words.clone()
@@ -157,17 +156,17 @@ class MultiSiteMH() :
         edit_rate = chosen_sentences[:,pos]!=sentences[:,pos]
         return chosen_sentences, edit_rate.float()
     
-    def mask_prob(self, position) :
+    def mask_prob(self, position, sentences):
         """
             Predict probability of words at mask position
-            This is the same as UniformGibbs.mask_prob
+            This is the same as UniformGibbs.mask_prob, except the input now includes sentences
             """
-        masked_sentences = self.sentences.clone()
+        masked_sentences = sentences.clone()
         masked_sentences[:, position] = self.mask_id
         outputs = self.model(masked_sentences)
         return F.softmax(outputs[0][:, position] / self.temp, dim = -1)
     
-    def get_total_likelihood(self) :
+    def get_total_likelihood(self):
         """
             This is the same as UniformGibbs.get_total_likelihood
             """
@@ -175,13 +174,40 @@ class MultiSiteMH() :
         
         # Why cut off first and last?
         for j in range(1, self.sentences.shape[1] - 1) :
-            probs = torch.log(self.mask_prob(j))
+            probs = torch.log(self.mask_prob(j,self.sentences))
             for i in range(self.sentences.shape[0]) :
                 # Look up probability of the actual word at this position
                 sent_probs[i, j] = probs[i, self.sentences[i, j]]
     
         # Sum log probs for each sentence in batch
         return torch.sum(sent_probs, axis=1)
+    
+    def get_joint_probability(self,sampling_sites,sentences,m=10):
+        if len(sampling_sites) == 1:
+            probs = self.mask_prob(sampling_sites.item(),sentences)
+            conditional = torch.tensor([prob[word].item() for prob,word in zip(probs,sentences[:,sampling_sites.item()])]).to(args.device)
+            return conditional
+        else:
+            #Random list for which site to factor out
+            random_list = torch.randperm(len(sampling_sites)).to(args.device)
+            joint_probs = torch.zeros(sentences.shape[0],len(sampling_sites)).to(args.device)
+            for iter_id, random_id in enumerate(random_list):
+                #Pick a site to factor out
+                site = sampling_sites[random_id]
+                #Calculate the conditional probability
+                probs = self.mask_prob(site,sentences)
+                conditional = torch.tensor([prob[word].item() for prob,word in zip(probs,sentences[:,site])]).to(args.device)
+                #Sample for Monte-Carlo
+                sampled_words = torch.tensor([list(torch.multinomial(prob, m, replacement=True).squeeze(-1)) for prob in probs]).to(args.device)
+                inv_prob_array = torch.zeros(sampled_words.shape).to(args.device)
+                for sample_iter in range(m):
+                    sampled_sentences = sentences.clone()
+                    sampled_sentences[:,site] = sampled_words[:,sample_iter]
+                    inv_prob_array[:,sample_iter] = torch.div(torch.ones(sentences.shape[0]).to(args.device),\
+                                                          self.get_joint_probability(sampling_sites[sampling_sites!=site],sampled_sentences))
+                joint_probs[:,iter_id] = torch.div(conditional,inv_prob_array.mean(dim=1))
+            return torch.log(joint_probs.mean(dim=1))
+            
 
 def run_chains(args) :
     # Load sentences
