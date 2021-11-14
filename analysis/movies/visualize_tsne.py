f31d275b12dfb26489f8d14cdd7b26050676633a
Robert Hawkins <hawkrobe@gmail.com>
2021-11-13 21:53:11 -0800

    clean up analysis folder

--- analysis/movies/visualize_tsne.py
+++ analysis/movies/visualize_tsne.py
@@ -3,10 +3,11 @@ import pandas as pd
 from sentence_transformers import SentenceTransformer
 
 model = SentenceTransformer('paraphrase-distilroberta-base-v1')
-chain = (pd.read_csv('../behavioral_experiment/stimuli/bert/12TokenSents.csv')
-         .query("chain_num == 0 and sentence_num == 6"))
+#chain = (pd.read_csv('../data/21TokenSentsAll/bert_new_gibbs_mixture_0.001_input_1.csv')
+chain = (pd.read_csv('../data/12TokenSentsMH/bert_new_mh_1_random_sweep_input_1.csv')
+         .query("chain_num == 0 and sentence_num in [2,6]"))
 sentences = chain['sentence'].str.replace(r'\[CLS\] ', '').str.replace(r' \[SEP\]', '').tolist()
-sentence_embeddings = model.encode(sentences)
+sentence_embeddings = model.encode(sentences, show_progress_bar=False)
 
 from sklearn.decomposition import PCA
 from sklearn.manifold import TSNE
@@ -16,6 +17,5 @@ tsne = TSNE(n_components = 2, n_iter=2000)
 big_pca = PCA(n_components = 50)
 
 tsne_vals = tsne.fit_transform(big_pca.fit_transform(sentence_embeddings))
-tsne = pd.concat([chain, pd.DataFrame(tsne_vals, columns = ['x_tsne', 'y_tsne'],index=chain.index)],
-                    axis = 1)
+tsne = pd.concat([chain, pd.DataFrame(tsne_vals, columns = ['x_tsne', 'y_tsne'],index=chain.index)], axis = 1)
 tsne.to_csv('tsne_out.csv')
