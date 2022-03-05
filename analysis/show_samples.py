a960298e72edcc583cf92951e86b36a09434d226
山越貴耀 <takateru@yamakoshinoMacBook-Pro.local>
2022-03-05 15:03:03 +0900

    updated show_samples

--- analysis/show_samples.py
+++ analysis/show_samples.py
@@ -1,22 +1,22 @@
 import pandas as pd
 import streamlit as st
 import numpy as np
-import plotly.graph_objects as go
 
 df = pd.read_csv('../behavioral_experiment/data/averaged_responses.csv')
+df = df.rename(columns={'initial_state_prob_class':'init_state'})
 col1, col2 = st.columns(2)
 with col1:
     sort_option = st.selectbox('sort by',
-                        ('phase', 'sent_length','prob','avg_response'))
+                        ('avg_response','prob','phase','sent_length','init_state','source'))
 with col2:
     sort_dir = st.selectbox('ascending or descending?',
                         ('ascending', 'descending'))
 
 sort_dir = sort_dir=='ascending'
 new_df = df.sort_values(by=sort_option,ascending=sort_dir).\
-            loc[lambda d: d['source']=='bert'].\
-            drop(columns=['step','source','sentence_id','initial_state_prob_class']).\
-            reindex(columns=['phase', 'sent_length','sentence','prob','avg_response'])
+            loc[lambda d: d['source']!='wiki'].\
+            drop(columns=['step','sentence_id']).\
+            reindex(columns=['source','init_state','phase', 'sent_length','sentence','prob','avg_response'])
 
 max_width = 1500
 padding_top = 2
